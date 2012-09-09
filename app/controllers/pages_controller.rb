6798b7775fde919b9811db86fc40f4f7db25c4ff
Nathaniel <nathanielescribano@gmail.com>
2012-09-08 22:48:28 -0700

    rough draft of fully working controller - there could be bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,29 +1,31 @@
 class PagesController < ActionController::Base
   protect_from_forgery
-
-  before_filter :set_api_key
+  include ApplicationHelper
 
   def index
-    @menu_item_data = get_menu_item_data
+    session[:max_price] ||= params[:max_price]
+    session[:selected_items] ||= []
+    session[:selected_items] << menu_item_data_from_param(params)
+    @selected_items = session[:selected_items]
   end
 
-  # this is bad - but its a hackathon so whatever
-  def set_api_key
-    api_key = '4b11590e2053dbe2b02adbc82c30f48dcf78dfec'
-    @lc = Locu::Client.new(api_key)
-  end
-
-  def get_menu_item_data
-    lc_result_hash  = @lc.menu.search_by({ :name => 'pizza',
-                                           :price__lte => '100' })
-    mi = MenuItem.new(lc_result_hash)
-    mi.item_list_with(['id', 'name', 'price'])
-  end
-  
   def search
-    @menu_item_data = []
-    @menu_item_data << {:id => "1", :name =>"Sushi", :price => "$1.00"}
-    @menu_item_data << {:id => "2", :name =>"Beer" ,:price => "$2.00"}
-    @menu_item_data << {:id => "3", :name =>"Pizza",:price => "$3.00"}
+    if params['menu_item'] && params['money_left']
+      lc = get_locu_client
+      @menu_item_data = menu_item_data
+    end
   end
+
+  def finalize
+    @max_price = session[:max_price]
+    @selected_items = session[:selected_items]
+
+    # all the selected_items should point to the same
+    # vendor. therefore, it doesn't matter which id
+    # we use
+    id = @selected_items.first['id']
+
+    @vendor = find_vendor_by_menu_item_id id
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
+# this is bad - but its a hackathon so whatever
 module ApplicationHelper
+
+  def get_locu_client
+    api_key = '4b11590e2053dbe2b02adbc82c30f48dcf78dfec'
+    lc = Locu::Client.new(api_key)
+  end
+
+  def menu_item_data(lc, search_hash)
+    lc_result_hash  = lc.menu.search_by({ :name => search_hash['menu_item'],
+                                          :price__lte => search_hash['money_left']})
+    mi = MenuItem.new(lc_result_hash)
+    mi.item_list_with(['id', 'name', 'price'])
+  end
+
+  def menu_item_data_from_param(p)
+    {:id => p[:id], :name => p[:name], :price => p[:price]}
+  end
+
+  def find_vendor_by_menu_item_id(id)
+    lc = get_locu_client
+    lc_result_hash  = lc.menu.search_by_id id
+    mi = MenuItem.new(lc_result_hash)
+    mi.find_venue_id_by_id(id)
+  end
+
 end
