efb7c0fc65ad42e749d7f178504d89d34828dc6b
Yash Ranadive <yash.ranadive@gmail.com>
2012-09-08 21:19:59 -0700

    front end shize

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ActionController::Base
 
   def index
   end
+  
+  def search
+    @menu_item_data = []
+    @menu_item_data << {:id => "1", :name =>"Sushi", :price => "$1.00"}
+    @menu_item_data << {:id => "2", :name =>"Beer" ,:price => "$2.00"}
+    @menu_item_data << {:id => "3", :name =>"Pizza",:price => "$3.00"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Cas::Application.routes.draw do
   root :to => "pages#index"
+  get 'pages/search'
 end
